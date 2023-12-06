a072c2de00451a6ad86e4c3ca9a362961202cdeb
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:21:36 +0100

    Update Servo-Motor-Module.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/Servo-Motor-Module.h
+++ src/Servo-Motor-Module.h
@@ -1,24 +1,15 @@
 #include <Servo.h>
 
-typedef struct setup2 {
-  int servo;
-  int angle;
-} ServoConfig;
+class Servo {
+  public:
+    int servo;
+    int angle;
+};
 
 Servo servo;
-ServoConfig pin1;
-ServoConfig pin2;
-ServoConfig pin3;
-ServoConfig pin4;
-ServoConfig pin5;
-ServoConfig pin6;
-ServoConfig pin7;
-ServoConfig pin8;
-ServoConfig pin9;
-ServoConfig pin10;
 
-void moveServo(int i, int angle) {
-  pin[i].angle = angle;
-  servo.attach(pin[i].servo);
+void Servo::move(Servo i, int angle) {
+  i.angle = angle;
+  servo.attach(i.servo);
   servo.write(angle);
 }
