afc599278baee829ae90bd1d81741505745c2379
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:50:50 +0100

    Update Example_Sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- examples/Example Sketch/Example_Sketch.ino
+++ examples/Example Sketch/Example_Sketch.ino
@@ -1,4 +1,13 @@
 #include <Servo-Motor-Module.h>
 
-ServoMotor example = {9, 27};
-example.move(example);
+void setup() {
+  ServoMotor example = {9, 7}; /* Define the servo */
+}
+
+void loop() {
+  example.move(example); */ Move the servo */
+  delay(2000); /* Wait 2 seconds */
+  example.angle = 57; /* Set a new angle */
+  example.move(); /* Move the servo */
+  delay(2000); /* Wait 2 seconds */
+}
