85b046167ff0d582367d8cf480265a63377e2ec6
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-10 11:49:31 +0100

    Update Servo-Motor-Module.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/Servo-Motor-Module.h
+++ src/Servo-Motor-Module.h
@@ -1,12 +1,13 @@
 #include <Servo.h>
 
-struct Servo2 {
+Servo servo;
+
+class Servo2 {
+  public:
     int servo;
     move(int angle);
     attach(int pin);
-} Servo2;
-
-Servo servo;
+};
 
 void Servo2::move(int angle) {
   this.angle = angle;
